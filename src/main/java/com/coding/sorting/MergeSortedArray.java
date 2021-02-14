d5b92384612dfaaaac1e089c542719edf71900ad
happysilvercold <happysilvercold@gmail.com>
2021-02-15 08:20:27 +0900

    2021-02-15 Sorting coding

--- src/main/java/com/coding/sorting/MergeSortedArray.java
+++ src/main/java/com/coding/sorting/MergeSortedArray.java
@@ -1,18 +1,18 @@
 package com.coding.sorting;
 
 /**
- * 문제 : 정렬된 배열 A와 B가 주어진다. A의 끝에는 B를 전부 넣을 수 있을 만큼 충분한 여유공간이 있다.
- * B와 A를 정렬된 상태로 병합하는 메서드를 작성하라.
+ * Problem
+ *  The ordered array A and array B are given.
+ *  There is enough free space at the end of A to accommodate all B.
+ *  Write a method that merges A and B into an ordered state.
+ *
  */
 public class MergeSortedArray {
 
     /**
-     * 문제
-     * @param a
-     * @param b
-     * @return
+     * resolution : make a new array
      */
-    public int[] merge(int[] a, int[] b){
+    public int[] mergeByMakingNewArray(int[] a, int[] b){
 
         int aIndex = 0;
         int bIndex = 0;
@@ -45,5 +45,27 @@ public class MergeSortedArray {
         }
 
         return mergeArray;
+    }
+
+    /**
+     * resolution : use a existing a array. insert from the end of a array
+     */
+    public int[] mergeByOriginArray(int[] a, int[] b, int lastALength, int lastBLength){
+        int aIndex = lastALength - 1;
+        int bIndex = lastBLength - 1;
+        int mergedIndex = lastALength + lastBLength-1;
+
+        while (bIndex >= 0){
+            if(aIndex >= 0 && a[aIndex] > b[bIndex]){
+                a[mergedIndex] = a[aIndex];
+                aIndex--;
+            }else{
+                a[mergedIndex] = b[bIndex];
+                bIndex--;
+            }
+            mergedIndex--;
+        }
+
+        return a;
     }
 }
--- src/test/java/com/coding/sorting/MergeSortedArrayTest.java
+++ src/test/java/com/coding/sorting/MergeSortedArrayTest.java
@@ -2,18 +2,37 @@ package com.coding.sorting;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MergeSortedArrayTest {
 
+
     @Test
-    void merge() {
+    void testMergeByMakingNewArray() {
         MergeSortedArray mergeSortedArray = new MergeSortedArray();
         int[] a = {1,3,4};
         int[] b = {2,3,5};
-        int[] mergeArray = mergeSortedArray.merge(a, b);
+        int[] mergeArray = mergeSortedArray.mergeByMakingNewArray(a, b);
 
         assert mergeArray.length == (a.length+b.length);
         assert mergeArray[0] == 1;
+        assert mergeArray[mergeArray.length-1] == 5;
+    }
+
+    @Test
+    void mergeByOriginArray() {
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        int[] a = new int[6];
+        a[0] = 1;
+        a[1] = 3;
+        a[2] = 4;
+        int[] b = {2,3,5};
+        int[] mergeArray = mergeSortedArray.mergeByOriginArray(a, b, 3, b.length);
+
+        assert mergeArray.length == (a.length);
+        assert mergeArray[0] == 1;
+        assert mergeArray[mergeArray.length-1] == 5;
     }
 }
